4138f068981c1124a6b08565d4026086055964dc
Anupam Biswas <anupambiswas997@gmail.com>
2025-08-28 10:15:59 +0000

    added index shuffler to randomly select entries from dataset

---
+++ include/index_shuffler.hpp
@@ -0,0 +1,16 @@
+#ifndef INDEX_SHUFFLER_HPP
+#define INDEX_SHUFFLER_HPP
+
+#include <vector>
+
+class IndexShuffler
+{
+    std::vector<size_t> m_indices;
+    bool m_doShuffle;
+public:
+    IndexShuffler(size_t size, bool doShuffle);
+    void update();
+    size_t getIndex(size_t i);
+};
+
+#endif
---
+++ src/index_shuffler.cpp
@@ -0,0 +1,32 @@
+#include "index_shuffler.hpp"
+#include <cstdlib>
+
+IndexShuffler::IndexShuffler(size_t size, bool doShuffle)
+{
+    m_indices = {};
+    for(size_t i = 0; i < size; i++)
+    {
+        m_indices.push_back(i);
+    }
+    m_doShuffle = doShuffle;
+}
+
+void IndexShuffler::update()
+{
+    if(!m_doShuffle)
+    {
+        return;
+    }
+    for(size_t i = 0; i < m_indices.size(); i++)
+    {
+        size_t i2 = rand() % m_indices.size();
+        size_t temp = m_indices[i];
+        m_indices[i] = m_indices[i2];
+        m_indices[i2] = temp;
+    }
+}
+
+size_t IndexShuffler::getIndex(size_t i)
+{
+    return m_indices[i];
+}
