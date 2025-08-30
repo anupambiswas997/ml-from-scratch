e7fcca6a32407e371488fa480a048c6202ff1d39
Anupam Biswas <anupambiswas997@gmail.com>
2025-08-30 21:05:35 +0000

    added generic GD steps

--- include/gradient_descent.hpp
+++ include/gradient_descent.hpp
@@ -3,17 +3,38 @@
 
 #include "vectr.hpp"
 
-class IGradientEvaluator
+// Gradient Descent is an iterative process. Each iteration of the process
+// may be summarized to be have the following:
+//   a. compute gradients of the cost function w.r.t.weights and bias
+//   b. update the weights and bias using the computed gradients and hyperparameters
+//   c. inspect if the conditions to discontinue the iterations have been achieved.
+// In the above steps, apart from step (a), other steps are quite generic. Step (a)
+// varies from problem to problem. For example, the method of computing the gradients
+// for Linear Regression and Logistic Regression can be different, as the former may
+// use ordinary least squares, and the latter may use log-loss.
+// To avoid having to repeat writing the logic for the generic parts, the task of
+// computing the gradients, and hence the increments of weights and bias, may be
+// delegated to a different class, here called IGDIncrementsEvaluator. Using this
+// interface class, only the variable step (a) will need to be written for different
+// models that require Gradient Descent solution to obtain weights and bias.
+
+class IGDIncrementsEvaluator
 {
 protected:
     Vector m_weightIncrements;
     double m_biasIncrement;
 public:
-    virtual void evaluateGradientAndIncrements() = 0;
-    virtual const Vector& getWeightIncrements();
-    virtual double getBiasIncrement();
+    virtual void evaluateIncrements() = 0;
+    virtual const Vector& getWeightIncrements()
+    {
+        return m_weightIncrements;
+    }
+    virtual double getBiasIncrement()
+    {
+        return m_biasIncrement;
+    }
 };
 
-void solveGradientDescent(IGradientEvaluator *gradEvaluator, size_t numWeights, double tolerance=1.0e-8, size_t maxNumIterations=100000);
+void solveGradientDescent(IGDIncrementsEvaluator *gdincEvaluator, size_t numWeights, double tolerance=1.0e-8, size_t maxNumIterations=100000);
 
 #endif
--- src/gradient_descent.cpp
+++ src/gradient_descent.cpp
@@ -1,6 +1,65 @@
 #include "gradient_descent.hpp"
+#include "random_quantities.hpp"
+#include <cmath>
 
-void solveGradientDescent(IGradientEvaluator *gradEvaluator, size_t numWeights, double tolerance, size_t maxNumIterations)
+void solveGradientDescent(IGDIncrementsEvaluator *gdincEvaluator, size_t numWeights, double tolerance, size_t maxNumIterations)
 {
-    //
+    // Initialize bias and weights.
+    double bias = getRandom();
+    Vector weights = getRandomVector(numWeights);
+
+    // Variables that will determine whether or not to continue iterating:
+    bool cond = true;
+    size_t iterationCount = 0;
+    double maxIncrement;
+    bool incrementCond;
+    bool iterCond;
+
+    // Gradient descent is performed with:
+    //   * entire X, when isStochastic = false
+    //   * randomly chosen numStochasticSamples entries of X, when isStochastic = true
+    //bool isStochastic = (numStochasticSamples > 0);
+
+    // Indexer object helps to:
+    //   * randomly select numStochasticSamples entries from X for stochastic GD (isStochastic = true)
+    //   * just get all entries in the case of batch GD (isStochastic = false)
+    //Indexer indexer(X.getNumRows(), isStochastic);
+
+    // For partial derivative calculations in every iteration of GD, the number
+    // of data entries (numRows) to take into account depends on whether stochastic or batch GD
+    // is being performed.
+    //size_t numRows = isStochastic ? numStochasticSamples : X.getNumRows();
+    //size_t numColumns = X.getNumColumns();
+    //double mInv = 1.0 / numRows;
+    //double mInvNegLR = -mInv * learningRate;
+
+    while(cond)
+    {
+        gdincEvaluator->evaluateIncrements();
+        const Vector& weightsIncrement = gdincEvaluator->getWeightIncrements();
+        double biasIncrement = gdincEvaluator->getBiasIncrement();
+        weights = weights + weightsIncrement;
+        bias += biasIncrement;
+
+        // Compute maximum error
+        double absMinWIncrement = fabs(weightsIncrement.getMin());
+        double absMaxWIncrement = fabs(weightsIncrement.getMax());
+        double wIncrement = (absMinWIncrement < absMaxWIncrement) ? absMinWIncrement : absMaxWIncrement;
+        double absBiasIncrement = fabs(biasIncrement);
+        maxIncrement = (wIncrement < absBiasIncrement) ? absBiasIncrement : wIncrement;
+
+        iterationCount++;
+        incrementCond = maxIncrement > tolerance;
+        iterCond = iterationCount < maxNumIterations;
+        // The looping to find optimal weights continues if:
+        //   a. the maximum error is still greater than the provided tolerance, and
+        //   b. the number of iterations is smaller than the provided iteration limit.
+        cond = incrementCond && iterCond;
+    }
+    /*
+    std::cout << "    Linear regression gradient descent finished with:" << std::endl;
+    std::cout << "    Number of iterations completed: " << iterationCount << std::endl;
+    std::cout << "    Maximum number of iterations: " << maxNumIterations << std::endl;
+    std::cout << "    Last absolute weights/bias increment: " << maxIncrement << std::endl;
+    std::cout << "    Error tolerance used: " << tolerance << std::endl;//*/
 }
