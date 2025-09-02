56ef62862222b073c336f8fee0a93cf375578f5e
Anupam Biswas <anupambiswas997@gmail.com>
2025-09-02 09:23:24 +0000

    added GD solver as virtual class

--- include/gradient_descent.hpp
+++ include/gradient_descent.hpp
@@ -52,4 +52,28 @@ typedef struct GDSolutionStruct
 
 GDSolution getGDSolution(IGDIncrementsEvaluator *gdincEvaluator, size_t numWeights, double tolerance=1.0e-8, size_t maxNumIterations=100000);
 
+class IGradientDescentSolver
+{
+protected:
+    // This class needs read access to source data and target.
+    const Matrix* m_pX;
+    const Vector* m_py;
+    Vector m_weights;
+    double m_bias;
+    Vector m_weightIncrements;
+    double m_biasIncrement;
+    size_t m_iterationCount;
+    size_t m_maxIterations;
+    double m_tolerance;
+    double m_maxIncrement;
+public:
+    IGradientDescentSolver(const Matrix& X, const Vector& y, size_t numIterations=10000, double tolerance=1.0e-8);
+    virtual void evaluateIncrements() = 0;
+    virtual bool shouldContinueIterating();
+    virtual void log() const;
+    virtual void solve();
+    const Vector& getWeights() const;
+    double getBias() const;
+};
+
 #endif
--- src/gradient_descent.cpp
+++ src/gradient_descent.cpp
@@ -1,6 +1,7 @@
 #include "gradient_descent.hpp"
 #include "random_quantities.hpp"
 #include <cmath>
+#include "matrix.hpp"
 
 GDSolution getGDSolution(IGDIncrementsEvaluator *gdincEvaluator, size_t numWeights, double tolerance, size_t maxNumIterations)
 {
@@ -45,4 +46,65 @@ GDSolution getGDSolution(IGDIncrementsEvaluator *gdincEvaluator, size_t numWeigh
     std::cout << "    Last absolute weights/bias increment: " << maxIncrement << std::endl;
     std::cout << "    Error tolerance used: " << tolerance << std::endl;//*/
     return GDSolution(weights, bias, iterationCount, maxIncrement);
+}
+
+IGradientDescentSolver::IGradientDescentSolver(const Matrix& X, const Vector& y, size_t numIterations, double tolerance)
+{
+    m_pX = &X;
+    m_py = &y;
+    m_maxIterations = numIterations;
+    m_tolerance = tolerance;
+}
+
+bool IGradientDescentSolver::shouldContinueIterating()
+{
+    // Compute maximum error
+    double absMinWIncrement = fabs(m_weightIncrements.getMin());
+    double absMaxWIncrement = fabs(m_weightIncrements.getMax());
+    double wIncrement = (absMinWIncrement < absMaxWIncrement) ? absMinWIncrement : absMaxWIncrement;
+    double absBiasIncrement = fabs(m_biasIncrement);
+    m_maxIncrement = (wIncrement < absBiasIncrement) ? absBiasIncrement : wIncrement;
+
+    bool incrementCond = m_maxIncrement > m_tolerance;
+    bool iterCond = m_iterationCount < m_maxIterations;
+    // The looping to find optimal weights continues if:
+    //   a. the maximum error is still greater than the provided tolerance, and
+    //   b. the number of iterations is smaller than the provided iteration limit.
+    return incrementCond && iterCond;
+}
+
+void IGradientDescentSolver::log() const
+{
+    // Nothing to log, by default
+}
+
+void IGradientDescentSolver::solve()
+{
+    // Initialize bias and weights.
+    m_bias = getRandom();
+    m_weights = getRandomVector(m_pX->getNumColumns());
+
+    // Variables that will determine whether or not to continue iterating:
+    bool cond = true;
+    m_iterationCount = 0;
+
+    while(cond)
+    {
+        evaluateIncrements();
+        m_weights = m_weights + m_weightIncrements;
+        m_bias += m_biasIncrement;
+        m_iterationCount++;
+        cond = shouldContinueIterating();
+        log();
+    }
+}
+
+const Vector& IGradientDescentSolver::getWeights() const
+{
+    return m_weights;
+}
+
+double IGradientDescentSolver::getBias() const
+{
+    return m_bias;
 }
