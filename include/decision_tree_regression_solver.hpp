65c1ce5eb5afb536ee7b5e2fbac7347f678728c5
Anupam Biswas <anupambiswas997@gmail.com>
2025-09-24 09:45:02 +0000

    added some comments

--- include/decision_tree_regression_solver.hpp
+++ include/decision_tree_regression_solver.hpp
@@ -3,6 +3,21 @@
 
 #include "base_solver.hpp"
 
+// DecisionTree is a tree data structure and the forms the core
+// of the decision tree solution process. Unlike weights and bias terms
+// in other machine learning models, the decision tree solver constructs
+// a tree data structure from the training data.
+// A DecisionTree object will either be:
+//   - an internal node which will branch into left and right nodes; or
+//   - a leaf node which will NOT branch into left and right nodes.
+// An internal node is associated with a condition based on:
+//   - a particular column of input vector; and
+//   - a split-value.
+// Basically the internal node's condition is as follows:
+// if input-vector[column] < split-value, follow left node, otherwise right.
+// A leaf node is a terminal node and is associated with a value, which is
+// used as the target for the input-vector.
+
 struct DecisionTree
 {
     size_t column;
@@ -20,7 +35,19 @@ struct DecisionTree
 };
 
 // DecisionTreeRegressionSolver is the class that can be used
-// to create a decision tree for a regression problem.
+// to create a decision tree for a regression problem. Basically
+// this solver contains a DecisionTree object which is constructed
+// during the training process.
+// Although decision tree solvers can have different types of
+// criteria for the leaf nodes, i.e. to stop further branching, this
+// particular implementation is based on the criterion of maximum leaf
+// size. That implies, there is a limit on the maximum number of input
+// vectors from which a leaf node's value should be determined.
+// In other words, during the training process, when a node is being
+// constructed with a subset of the training data, if the size of the
+// subset is greater than the maximum leaf size, the node gets to have
+// left and right branching. Otherwise, it is associated with a target
+// value equal to the mean of the target values associated with subset. 
 
 class DecisionTreeRegressionSolver: virtual public BaseSolver
 {
